46378fc7136d187f3a7c54a0016c21b1529e04ca
Andrew Kirik <akirik@kirikart.com>
2017-10-10 14:56:40 +0100

    more comments

--- main.c
+++ main.c
@@ -6,14 +6,11 @@
  * 
  * Blinking LED
  * 
- *  Board connection (PICKit 2 Low Count Demo):
  *   PIN                	Module                         				  
  * -------------------------------------------                        
- *    RC0 (DS1; J1 -> 10)     	LED
+ *   RC0		     	LED (+)
  *
  */
-
-#include <xc.h>
 
 /* The __delay_ms() function is provided by XC8. 
 It requires you define _XTAL_FREQ as the frequency of your system clock. 
@@ -25,36 +22,39 @@ begins with a single underscore.
 */
 #define _XTAL_FREQ 4000000ul  
 
+#include <xc.h>
+
 // CONFIG
 // PIC16F690 Configuration Bit Settings
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA4/OSC2/CLKOUT and RA5/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = ON       // MCLR Pin Function Select bit (MCLR pin function is MCLR)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Selection bits (BOR enabled)
-#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode is enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config FOSC 	= HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA4/OSC2/CLKOUT and RA5/OSC1/CLKIN)
+#pragma config WDTE 	= OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE 	= OFF      	// Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE 	= ON       	// MCLR Pin Function Select bit (MCLR pin function is MCLR)
+#pragma config CP 		= OFF       // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD 		= OFF       // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN 	= ON       	// Brown-out Reset Selection bits (BOR enabled)
+#pragma config IESO 	= ON        // Internal External Switchover bit (Internal External Switchover mode is enabled)
+#pragma config FCMEN 	= ON       	// Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
 
-
+// INIT
 void sys_init()
 {
-  OSCCON=0x70;      // Select 8 Mhz internal clock
-  ANSEL = 0;        // Set PORT ANS0 to ANS7 as Digital I/O
-  ANSELH = 0;       // Set PORT ANS8 to ANS11 as Digital I/O
-  TRISC = 0x00;     // Set All on PORTC as Output    
-  PORTC = 0;        // Set PORTC all 0
+  ANSEL 	= 0x00;     // Set PORT ANS0 to ANS7 as Digital I/O
+  ANSELH 	= 0x00;		// Set PORT ANS8 to ANS11 as Digital I/O
+  TRISC 	= 0x00;     // Set All on PORTC as Output    
+  PORTC 	= 0x00;     // Set PORTC all 0
 }
 
+// MAIN
 void main(void) 
 {
     sys_init();
     
-    while(1)  {
+    while(1)  
+	{
         PORTC ^= 0x01;      // flip RC0
         __delay_ms(1000);   // sleep 1 seconds
     }
     
-  return;
+	return;
 }
